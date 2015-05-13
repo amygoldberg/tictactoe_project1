72132e0a6e2987d312e8e1750ab35c568f48ecfa
amygoldberg <amykgoldberg@gmail.com>
2015-05-13 17:41:38 -0400

    committing working game with restart button and tie game comment

--- js/tictactoe2.js
+++ js/tictactoe2.js
@@ -1,86 +1,91 @@
-//restart button
-
 // scoreboard - keep track of how many times X and O has won games
 
 // README - write doc
 
-var player = 0;
-var players = ['X', 'O'];
-var squareArray = [];
-var count = 0;
-var gameOver = false;
+//run jshint on this before turning in
+
 
 $(document).ready(function() {
+
+  var player = 0;
+  var players = ['X', 'O'];
+  var squareArray = [];
+  var count = 0;
+  var gameOver = false;
+
+  var restart = function() {
+    $('.square').text("");
+    count = 0;
+  };
+
+  $('#new-game').on('click', function() {
+    restart();
+  });
+
   $('.square').click(function() {
     squareArray=[];
     if ($(this).html() === '') {
       $(this).html(players[player]);
 
-    count++;
-
-    player = 1 - player;
+      count++;
 
       $('.square').each(function() {
-          squareArray.push($(this).html())
+        squareArray.push($(this).html())
       });
 
       console.log(squareArray);
 
-      if (getWinner(squareArray)) {
-        if(player === 1) {
-          $('#winner').show();
-          $('#winner').html('X wins!');
-          // alert('X wins!')
-        }
-        else if (player === 0) {
-          $('#winner').show();
-          $('#winner').html('O wins!');
-          //alert('O wins!')
-        }
+      if (getWinner(squareArray, players[player])) {
+
+        $('#winner').html(players[player] + ' wins!');
+        $('#winner').show();
+
       } else if (count === 9) {
-          gameOver = true;
-          $('#tie').show();
-          $('#tie').html('Tie game!');
+        gameOver = true;
+        $('#tie').show();
+        $('#tie').html('Tie game!');
       }
+
+      player = 1 - player;
+
     }
-  })
-})
+  });
 
-function getWinner(square) {
-  var win;
-  for (var i = 0; i < players.length; i += 1) {
+  function getWinner(square, player) {
+    var win;
 
-    if (square[0] === players[i] && square[1] === players[i] && square[2] === players[i]) {
+    if (square[0] === player && square[1] === player && square[2] === player) {
       win = true;
     }
 
-    else if (square[3] === players[i] && square[4] === players[i] && square[5] === players[i]) {
+    else if (square[3] === player && square[4] === player && square[5] === player) {
       win = true;
     }
 
-    else if (square[6] === players[i] && square[7] === players[i] && square[8] === players[i]) {
+    else if (square[6] === player && square[7] === player && square[8] === player) {
       win = true;
     }
 
-    else if (square[0] === players[i] && square[3] === players[i] && square[6] === players[i]) {
+    else if (square[0] === player && square[3] === player && square[6] === player) {
       win = true;
     }
 
-    else if (square[1] === players[i] && square[4] === players[i] && square[7] === players[i]) {
+    else if (square[1] === player && square[4] === player && square[7] === player) {
       win = true;
     }
 
-    else if (square[2] === players[i] && square[5] === players[i] && square[8] === players[i]) {
+    else if (square[2] === player && square[5] === player && square[8] === player) {
       win = true;
     }
 
-    else if (square[0] === players[i] && square[4] === players[i] && square[8] === players[i]) {
+    else if (square[0] === player && square[4] === player && square[8] === player) {
       win = true;
     }
 
-    else if (square[2] === players[i] && square[4] === players[i] && square[6] === players[i]) {
+    else if (square[2] === player && square[4] === player && square[6] === player) {
       win = true;
     }
-  }
-  return win;
-};
+    return win;
+  };
+
+});
